1451ade7efdc1645c87b9f2740e6339beacc75a3
Kola Kolade <kolakolade@gmail.com>
2023-04-08 23:23:13 +0100

    Add CartContainer and CartItem components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { CartIcon } from '../icons';
 
 const Navbar = () => {
-  const { amount } = useSelector((state) => state.cart);
+  const { amount } = useSelector((store) => store.cart);
 
   return (
     <nav>
       <div className="nav-center">
         <h3>Redux Toolkit</h3>
         <div className="nav-container">
-          {/* <CartIcon /> */}
+          <CartIcon />
           <div className="amount-container">
             <p className="total-amount">{amount}</p>
           </div>
